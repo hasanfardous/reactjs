023e5413dbd25a1ba00bb124c06f10e22fb31157
hasanfardous <frworkbd@gmail.com>
2019-07-31 08:30:54 +0600

    Adding state and use that to our project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    siteTitle: 'This is our site title',
+    products: [
+      {
+        id: 1,
+        name: 'First product',
+        desc: 'This is product description'
+      },
+      {
+        id: 2,
+        name: 'Second product',
+        desc: 'This is product description'
+      },
+      {
+        id: 3,
+        name: 'Third product',
+        desc: 'This is product description'
+      },
+    ]
+  }
+  
+  render() {
+    return (
+      <div>
+        <Header/>
+        {console.log(this.state.products)}
+        <h2>{ this.state.siteTitle }</h2>
+
+        <h3>Products</h3>
+        {
+          this.state.products.map((product) => (
+            <li key={product.id}>Id: {product.id}, Name: {product.name}</li>
+          ))
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
